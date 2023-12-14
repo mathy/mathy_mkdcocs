5c6b273772cd4b91bbc8489bd90b4f3b1915789f
Justin DuJardin <justindujardin@users.noreply.github.com>
2023-12-14 15:14:28 -0800

    chore: fix deps in package
    needs mathy_core and mathy_envs

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/justindujardin/mathy",
     packages=setuptools.find_packages(),
-    install_requires=["svgwrite"],
+    install_requires=["svgwrite", "mathy_core", "mathy_envs"],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
